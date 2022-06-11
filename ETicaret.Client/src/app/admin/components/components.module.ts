99bb1c99f23efae99857402038f521de568e619f
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-06-11 18:12:31 +0300

    import added

--- ETicaret.Client/src/app/admin/components/components.module.ts
+++ ETicaret.Client/src/app/admin/components/components.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CustomerModule } from './customer/customer.module';
+import { ProductsModule } from './products/products.module';
+import { OrderModule } from './order/order.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    CustomerModule,
+    ProductsModule,
+    OrderModule,
+    DashboardModule
   ]
 })
 export class ComponentsModule { }

044acec4b25b62a8dd29b62dbaa2970e252d7176
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-06-11 16:43:56 +0300

    component added

---
+++ ETicaret.Client/src/app/admin/layout/layout.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './layout.component';
+
+/*
+Bir modul baska modulu içine alacaksa import etmesi gerekir.
+Alttakiler üst module dahil edilir yani
+Layoutu adminin içine girip importa ekleyeceğiz . admini de app module ye yani bir üst modulun içerisine
+import etmemiz gerekecek.
+Declare işlemi ise kendine en yakın module kendini declare eder.
+*/
+
+@NgModule({
+  declarations: [
+    LayoutComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LayoutModule { }

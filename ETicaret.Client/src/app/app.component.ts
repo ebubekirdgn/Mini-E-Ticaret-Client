fc87dac7f72fff34144ed2e82f1389f5c2d66207
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-07-01 04:02:53 +0300

    CustomToastrService added

--- ETicaret.Client/src/app/app.component.ts
+++ ETicaret.Client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
 declare var $: any
 @Component({
   selector: 'app-root',
@@ -8,7 +9,8 @@ declare var $: any
 })
 export class AppComponent {
   title = 'ETicaret.Client';
-  constructor(private toastr: ToastrService) {
-    this.toastr.success('Hello world!', 'Toastr fun!');
+  constructor(private toastrService: CustomToastrService) {
+    toastrService.message("Merhaba","Ebubekir Dgn",{messageType:ToastrMessageType.Info,
+    position:ToastrPosition.TopRight});
   }
 }

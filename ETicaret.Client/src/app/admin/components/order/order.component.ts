0ffdd8a068c7585f622f42390d6e27d56ca3df76
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-07-03 21:47:17 +0300

    order updated

--- ETicaret.Client/src/app/admin/components/order/order.component.ts
+++ ETicaret.Client/src/app/admin/components/order/order.component.ts
@@ -1,22 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from "ngx-spinner";
+import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.scss']
 })
-export class OrderComponent implements OnInit {
+export class OrderComponent extends BaseComponent implements OnInit {
 
-  constructor(private spinner: NgxSpinnerService) { }
-
-  ngOnInit(): void {
-    this.spinner.show("s1");
-    
-    setTimeout(() => {
-      /** spinner ends after 5 seconds */
-      this.spinner.hide("s1");
-    }, 5000);
+  constructor(spinner: NgxSpinnerService) { 
+    super(spinner)
   }
 
+  ngOnInit(): void {
+    this.showSpinner(SpinnerType.BallScaleMultiple)
+  }
 }

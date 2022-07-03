106d9f40310e7f0b9880d06cb2f1720d2d088724
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-07-04 00:17:27 +0300

    latest worked it

--- ETicaret.Client/src/app/ui/components/products/products.component.ts
+++ ETicaret.Client/src/app/ui/components/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 
 @Component({

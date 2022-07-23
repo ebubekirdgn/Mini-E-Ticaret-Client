da854eae3a14a37130495b778a4d24ecbd5e03dd
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-07-24 00:00:01 +0300

    update

--- ETicaret.Client/src/app/services/common/product/models/product.service.ts
+++ ETicaret.Client/src/app/services/common/product/models/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClientService } from '../../http-client.service';
 import { Create_Product } from 'src/app/contracts/create_product';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +14,16 @@ export class ProductService {
    this.httpClientService.post({
       controller:"products"
    },product)
-   .subscribe(result =>{
+    .subscribe(result => {
     successCallBack();
+   }, (errorResponse: HttpErrorResponse) => {
+      const _error: Array<{ key: string, value: Array<string> }> = errorResponse.error;
+        let message = "";
+        _error.forEach((v, index) => {
+          v.value.forEach((_v, _index) => {
+            message += `${_v}<br>`;
+          });
+        });
    });
   }
 }
